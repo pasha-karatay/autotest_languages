dcdcf2d2f6ae0f5328ffa61fe6acf1ababfda78d
pasha-karatay <pasha.karatay1988@gmail.com>
2021-08-22 15:09:26 +0300

    Добавил ассерт

--- test_items.py
+++ test_items.py
@@ -1,4 +1,5 @@
 def test_guest_should_see_login_link_pass(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    browser.find_element_by_css_selector("#add_to_basket_form > button")
+    button = len(browser.find_elements_by_css_selector("#add_to_basket_form > button"))
+    assert button > 0, 'Кнопка не найдена!!!'
